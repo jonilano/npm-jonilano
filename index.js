b12bbf12566d14e28e368208c9d618a9a3de5533
Jonathan Ilano <jonathan.ilano@gmail.com>
2016-06-13 00:59:15 +0800

    Update github link

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   birthday: '1981-11-23',
   twitter: 'https://twitter.com/jonilano',
-  github: 'https://github.com/kentcdodds',
+  github: 'https://github.com/jonilano',
   website: 'http://jonathanilano.com',
   email: 'jonathan.ilano@gmail.com'
 }
