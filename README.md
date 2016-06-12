e1a1c503d6f8c6ce80d70242a455349aeb0b6bb3
Jonathan Ilano <jonathan.ilano@gmail.com>
2016-06-13 00:32:34 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# jonilano
+
+me
+
+## Usage
+
+```javascript
+var jonilano = require('jonilano')
+console.log(jonilano.name.first) // 'Jonathan'
+// ... etc
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: {
+    first: 'Jonathan',
+    middle: 'Dabuet',
+    last: 'Ilano',
+  },
+  birthday: '1981-11-23',
+  twitter: 'https://twitter.com/jonilano',
+  github: 'https://github.com/kentcdodds',
+  website: 'http://jonathanilano.com',
+  email: 'jonathan.ilano@gmail.com'
+}
